4771462b6d17bba92586bc9b3bd53397e6d6f0e8
jolivem <jolivet.michel@free.fr>
2019-08-14 21:10:38 +0200

    wisiwig viewer OK

---
+++ src/Roadkill.Web/Assets/Scripts/jquery/tui-editor-extYoutube.js
@@ -0,0 +1,17 @@
+﻿
+var Editor = tui.Editor;
+Editor.defineExtension('youtube', function () {
+    Editor.codeBlockManager.setReplacer('youtube', function (youtubeId) {
+        var wrapperId = 'yt' + Math.random().toString(36).substr(2, 10);
+        setTimeout(renderYoutube.bind(null, wrapperId, youtubeId), 0);
+
+        return '<div id="' + wrapperId + '"></div>';
+    });
+});
+
+function renderYoutube(wrapperId, youtubeId) {
+    var el = document.querySelector('#' + wrapperId);
+    //el.innerHTML = '<iframe width="420" height="315" src="https://www.youtube.com/embed/' + youtubeId + '"></iframe>';
+    el.innerHTML = '<div> TOTO TATA TITI ' + youtubeId + '</div > ';
+}
+
